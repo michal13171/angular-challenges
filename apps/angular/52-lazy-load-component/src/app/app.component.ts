6035746eefca6303080928d20af71fef4802ac01
michal <michal131711@gmail.com>
2026-07-06 10:41:22 +0200

    feat(Answer:52): use defer for loaded cmp

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,25 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { PlaceholderComponent } from './placeholder.component';
 import { TopComponent } from './top.component';
 
 @Component({
   selector: 'app-root',
+  imports: [PlaceholderComponent, TopComponent],
+  standalone: true,
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
-        <app-top />
-      } @else {
+      <button
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        #loadBtn>
+        Load Top
+      </button>
+      @defer (on interaction(loadBtn); prefetch on hover(loadBtn)) {
+        <app-top></app-top>
+      } @placeholder {
         <app-placeholder />
-        <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
-          Load Top
-        </button>
+      } @error {
+        <!-- Fallback safe UI state if the network disconnects or the chunk fails to load -->
+        <p style="color: red;">
+          Failed to load the component. Please check your connection.
+        </p>
       }
     </div>
   `,
-  imports: [TopComponent, PlaceholderComponent],
 })
-export class AppComponent {
-  topLoaded = signal(false);
-}
+export class AppComponent {}
