f6ea0be12afe34e5670b3440fd1fef511a1a3d66
michal <michal131711@gmail.com>
2026-07-07 16:43:30 +0200

    feat(Answer:60): redirectTo: remove navigate into the routes in redirectTo

--- apps/angular/60-async-redirect/src/app/dashboard.ts
+++ apps/angular/60-async-redirect/src/app/dashboard.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { UserProfileService } from './user-profile.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
+  imports: [RouterOutlet, RouterLink],
+  standalone: true,
   template: `
     <div class="mb-4 flex gap-2">
       <button
@@ -12,23 +13,12 @@ import { UserProfileService } from './user-profile.service';
         Profile Page
       </button>
       <button
-        class="rounded border border-black bg-white px-4 py-2 text-black hover:bg-gray-200"
-        (click)="navigate()">
+        class="rounded border border-black bg-white px-4 py-2 text-black hover:bg-gray-200">
         User Page
       </button>
     </div>
     <router-outlet />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, RouterLink],
 })
-export class Dashboard {
-  private router = inject(Router);
-  private userProfile = inject(UserProfileService);
-
-  navigate() {
-    this.userProfile.getProfile().subscribe((profile) => {
-      void this.router.navigate(['/', profile]);
-    });
-  }
-}
+export class Dashboard {}
--- apps/angular/60-async-redirect/src/app/routes.ts
+++ apps/angular/60-async-redirect/src/app/routes.ts
@@ -1,16 +1,28 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
 import { AdminPage } from './admin-page';
 import { App } from './app';
 import { Dashboard } from './dashboard';
 import { ProfilePage } from './profile-page';
 import { UserPage } from './user-page';
+import { UserProfileService } from './user-profile.service';
 
 export const routes: Routes = [
   {
     path: '',
     component: App,
     children: [
-      { path: '', pathMatch: 'full', component: Dashboard },
+      {
+        path: '',
+        pathMatch: 'full',
+        component: Dashboard,
+        redirectTo: () => {
+          const userProfile = inject(UserProfileService);
+          userProfile.getProfile().subscribe((profile) => profile);
+
+          return 'profile';
+        },
+      },
       { path: 'profile', component: ProfilePage },
       { path: 'admin', component: AdminPage },
       { path: 'user', component: UserPage },
